0587bf1d1f17c10a5b63d65db60189b8a8c32681
cormac <142530101+CormacStone@users.noreply.github.com>
2024-09-03 14:06:42 -0600

    Create subtraction.java

---
+++ src/subtraction.java
@@ -0,0 +1,34 @@
+//owen charles 
+public class subtraction {
+    public int  options[] = new int[4];
+    public int correctoption;
+    public String text;
+    
+    {
+        // generate two numbwers and create text of questions
+        int numb1 = (int)(Math.random() * 50 + 0);
+        int numb2 = (int)(Math.random() * 50 + 0);
+        text = "what is " + numb1 + " - " + numb2 + "?";
+        
+        // pick random slot to store correct answer
+        correctoption = (int)(Math.random() * 4 - 0);
+        
+        // assign answers to array
+        for(int i = 0; i<options.length; i++){
+             if (i != correctoption){
+                 options[i] = (int)(Math.random() * 50 + -50);
+             }else{
+                 options[i] = numb1 - numb2;
+            }
+        }
+        
+    }
+    
+    public void printoptions(){
+        for (int i = 0; i < this.options.length; i++) {
+            System.out.println(i + ") " + options[i]);
+        }
+        
+    }
+    
+}
