2acbd91427a4e5bdcd6f07f1046c14777fde5354
AidanSlovik <158332992+AidanSlovik@users.noreply.github.com>
2024-11-12 12:59:34 -0700

    Update socialMedia.java

--- src/socialMedia.java
+++ src/socialMedia.java
@@ -1,3 +1,9 @@
 abstract class SocialMedia extends spread{
-    
+    private int likeToDislikeRation;
+    private int shareRatio;
+    private enum platform (
+        Tacebook, 
+        TikTok, 
+        Instagram, 
+    )
 }
