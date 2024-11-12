34e2bcc4c6026daf3fe593fa9f94b42df0156ebf
Tz3t <142743200+Tz3t@users.noreply.github.com>
2024-11-12 12:55:23 -0700

    Update dance.java

--- src/dance.java
+++ src/dance.java
@@ -1,3 +1,11 @@
 public class Dance extends Trends{
     private int lengthOfDance;
+    enum type{
+        country,
+        hipHop,
+        jazz, 
+        folk,
+        
+        
+        public 
 }
