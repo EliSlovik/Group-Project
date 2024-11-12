0f15f6879a5281c5a835f30da70115f57baf75cb
AidanSlovik <158332992+AidanSlovik@users.noreply.github.com>
2024-11-12 12:56:38 -0700

    Update magazine.java

--- src/magazine.java
+++ src/magazine.java
@@ -2,5 +2,8 @@ public class Magazine extends realLife{
     private int pages;
     private int numberOfCopes;
     private boolean isFrontPage;
-    
+    enum location {
+        bookStore, 
+        gasStation
+    }
 }
