e02bae69361414bbdb166a733232fb288a9ff28f
EliSlovik <142624510+EliSlovik@users.noreply.github.com>
2024-11-14 12:07:14 -0700

    Update food.java

--- src/food.java
+++ src/food.java
@@ -1,7 +1,7 @@
 public class Food extends Trends{
     private boolean isHealthy;
     private boolean isVegan;
-    
+    private boolean hasProtein;
     enum cuisine {
         Mexican,
         Asian,
