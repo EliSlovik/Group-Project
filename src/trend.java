73512c28d800143795141ae60ffc47000d7136b6
JialaiY <142963141+JialaiY@users.noreply.github.com>
2024-11-14 13:40:17 -0700

    Update trend.java

--- src/trend.java
+++ src/trend.java
@@ -1,8 +1,14 @@
-puclic abstract class trend {
+abstract class trend {
   double spreadRate;
   int numUpgrades;
   int peopleSeen;
   int numUpgrades;
+  
+  public abstract void trendSound();
+  // Regular method
+  public void sleep() {
+    System.out.println("Zzz");
+  }
   
   private void modify(){
     
