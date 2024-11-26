7e23936892d8cd1b9dc48126959c310928de54d3
JialaiY <142963141+JialaiY@users.noreply.github.com>
2024-11-25 20:57:47 -0700

    Update and rename Painting.pde to Fish.java

---
+++ AbstractEx/Fish.java
@@ -0,0 +1,23 @@
+public class Fish extends Animal {
+    private boolean isFreshwater;
+
+    public Fish(String name, boolean isFreshwater) {
+        super(name);
+        this.isFreshwater = isFreshwater;
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println(getName() + " the fish burps!");
+    }
+
+    @Override
+    public void move() {
+        String waterType = isFreshwater ? "freshwater" : "saltwater";
+        System.out.println(getName() + " swims in " + waterType + ".");
+    }
+
+    public boolean isFreshwater() {
+        return isFreshwater;
+    }
+}
--- AbstractEx/Painting.pde
+++
@@ -1,6 +0,0 @@
-// Concrete class Painting, subclass of Art
-class Painting extends Art {
-  void display() {
-    println("Painting is the practice of applying pigment to a surface to create a visual artwork.");
-  }
-}
