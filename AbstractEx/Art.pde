79b10d4367c95cd40a83701768b93694eb61e23c
JialaiY <142963141+JialaiY@users.noreply.github.com>
2024-11-25 20:57:24 -0700

    Update and rename Art.pde to Dog.java

--- AbstractEx/Art.pde
+++
@@ -1,5 +0,0 @@
-// Abstract class Art
-abstract class Art {
-  // Abstract method to be implemented by subclasses
-  abstract void display();
-}
---
+++ AbstractEx/Dog.java
@@ -0,0 +1,22 @@
+public class Dog extends Animal {
+    private String breed;
+
+    public Dog(String name, String breed) {
+        super(name);
+        this.breed = breed;
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println(getName() + " the " + breed + " barks!");
+    }
+
+    @Override
+    public void move() {
+        System.out.println(getName() + " the " + breed + " runs on four legs.");
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+}
