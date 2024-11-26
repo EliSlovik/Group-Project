3edba561abe8ef72bd3b7466e38d295820b1e440
EliSlovik <142624510+EliSlovik@users.noreply.github.com>
2024-11-26 13:07:48 -0700

    Update Animal.java

--- AbstractEx/Animal.java
+++ AbstractEx/Animal.java
@@ -1,14 +1,17 @@
 public abstract class Animal {
     private String name;
-
-    public Animal(String name) {
+    private int weight;
+    public Animal(String name, int weight) {
         this.name = name;
+        this.weight = weight;
     }
 
     public String getName() {
         return name;
     }
-
+    public int getWeight() {
+        return weight;
+    }
     public abstract void makeSound();
     public abstract void move();
 }
